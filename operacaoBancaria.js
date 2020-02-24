cc5d079705a07c357364f41ebe0db9d983ec5024
Miguel Lima <miguelsousalima@hotmail.com>
2020-02-24 19:07:05 -0300

    Challenge 01 - Add opetacoesBancarias

--- operacaoBancaria.js
+++ operacaoBancaria.js
@@ -1,5 +1,65 @@
 const user = {
   name: 'Miguel',
-  transation: [],
-  balance: 0
+  transactions: [],
+  balance: 0,
+  
 }
+
+function createTransaction(transaction) {
+  user.transactions.push(transaction);
+
+  if (transaction.type === 'credit') {
+    user.balance = user.balance + transaction.value;
+  } else if(transaction.type === 'debit') {
+    user.balance = user.balance - transaction.value;
+  }
+}
+
+function getHigherTransactionByType(type) {
+  let higherTransaction;
+  let higherValue = 0;
+
+  for (let transaction of user.transactions) {
+    if ( transaction.type == type && transaction.value > higherValue ){
+        higherValue = transaction.value;
+        higherTransaction = transaction;
+      }
+    }
+
+  return higherTransaction;
+} 
+
+function getAverageTransactionValue() {
+  let sum = 0;
+
+  for(let transaction of user.transactions) {
+    sum += transaction.value;
+  }
+
+  return sum / user.transactions.length;
+}
+
+function getTransactionsCount() {
+  let countCredit = 0;
+  let countDebit = 0;
+
+  for(let transaction of user.transactions) {
+    if(transaction.type == 'credit'){
+      countCredit++;
+    } else
+      countDebit++;
+  }
+
+  return {countCredit, countDebit}
+}
+
+createTransaction({ type: "credit", value: 250 });
+createTransaction({ type: "credit", value: 120 });
+createTransaction({ type: "debit", value: 80 });
+createTransaction({ type: "debit", value: 150 });
+
+console.log(user.balance);
+console.log(getHigherTransactionByType('credit'));
+console.log(getHigherTransactionByType('debit'));
+console.log(getAverageTransactionValue());
+console.log(getTransactionsCount());
